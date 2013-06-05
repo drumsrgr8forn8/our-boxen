44bd351bcbd434f20a568a9cd22a5b724cd34a1a
Nate West <natescott.west@gmail.com>
2013-06-05 11:34:20 -0500

    added personal manifest for drumsrgr8forn8

---
+++ modules/people/manifests/drumsrgr8forn8.pp
@@ -0,0 +1,14 @@
+class people::drumsrgr8forn8 {
+  include virtualbox
+  include skitch
+  include postgresql
+  include chrome
+  include iterm2
+  include sublime_text_2
+  include erlang
+  include hipchat
+  include kindle
+  include vlc
+  include skype
+  include zsh
+}
